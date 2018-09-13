324ee3391265099fcbe1481b862d57e1e7c7269b
cd <coraldarling@outlook.com>
2018-09-13 18:23:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |day, supplies|
       bbq_holiday = []
-      if (supplies).include?("BBQ") == true  |day|
+      if (supplies).include?("BBQ") == true
         
         bbq_holiday << day
       end
